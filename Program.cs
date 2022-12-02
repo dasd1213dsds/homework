e3161340843ce606c2ae22cd290b492ba478de93
aboba <aboba@aboba.gg>
2022-12-03 00:22:05 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine()!);
+int max = num1;
+if (num1<num2)
+    max=num2;
+Console.WriteLine("Максимальное число ровно");
+System.Console.Write(max);
